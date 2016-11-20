37224dbf5d87d301b307743e7d91ce531b6ac643
kpraharsha <kpraharsha@gmail.com>
2016-11-20 17:21:20 -0500

    cachematrix.R solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function is makeCacheMatrix() - it creates
+## a matrix that can cache its inverse.
+## The second function is cacheSolve() - it returns the
+## the inverse of matrix computed by makeCacheMatrix().
+## In case the inverse is already computed and the matrix
+## is the same as the created matrix, it'll retrieve the
+## inverse of the matrix from cache thereby decreasing the
+## computation time.
 
-## Write a short comment describing this function
+## makeCacheMatrix returns a list that contains functions to
+## 1. Set the value of the matrix.
+## 2. Get the value of the matrix.
+## 3. Set the inverse of the matrix.
+## 4. Get the inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inverse1 <- NULL
+set <- function(y){
+x <<- y
+inverse1 <<- NULL
+}
+get <- function() x
+setinverse <- function(inverse) inverse1 <<- inverse
+getinverse <- function() inverse1
+list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of the matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+inverse1 <- x$getinverse()
+
+## Check if inverse has already been computed. If it is
+## true, it gets the result. Else, sets the value using
+## setinverse function
+if(!is.null(inverse1)){
+message("getting cached data")
+return(inverse1)
 }
+data <- x$get()
+inverse1 <- solve(data)
+x$setinverse(inverse1)
+inverse1
+}
+
+## Test:
+## a = rbind(c(-1/2,3),c(3,-1/2))
+## b = makeCacheMatrix(a)
+## b$get()
+##      [,1] [,2]
+## [1,] -0.5  3.0
+## [2,]  3.0 -0.5
+## cacheSolve(b)
+##            [,1]       [,2]
+## [1,] 0.05714286 0.34285714
+## [2,] 0.34285714 0.05714286
+## cacheSolve(b)
+## getting cached data
+##            [,1]       [,2]
+## [1,] 0.05714286 0.34285714
+## [2,] 0.34285714 0.05714286
